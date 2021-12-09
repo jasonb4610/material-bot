5b8f4ab9c818682c212f2bc610f5e3203c6fc1ae
Jason Bell <jabell4610@gmail.com>
2021-12-09 22:25:18 +0000

    Initial application commit

---
+++ generated-conf/loadDatabase.php
@@ -0,0 +1,13 @@
+<?php
+$serviceContainer = \Propel\Runtime\Propel::getServiceContainer();
+// The below init method is here for reference but will be updated once the appropriate database tables have been added 
+/*
+$serviceContainer->initDatabaseMaps(array (
+  'default' => 
+  array (
+    0 => '\\MaterialBot\\Map\\AuthorTableMap',
+    1 => '\\MaterialBot\\Map\\BookTableMap',
+    2 => '\\MaterialBot\\Map\\PublisherTableMap',
+  ),
+));
+*/
